677784e96c543f4fcbbeb084d71bb80102289176
MoonCheung <salvador23@163.com>
2019-12-27 00:13:47 +0800

    feat: 评论列表

--- controller/comment.js
+++ controller/comment.js
@@ -4,20 +4,53 @@
  * @Github: https://github.com/MoonCheung
  * @Date: 2019-12-12 22:16:32
  * @LastEditors: MoonCheung
- * @LastEditTime: 2019-12-13 00:59:42
+ * @LastEditTime: 2019-12-26 22:50:01
  */
 
 const commentModel = require('../models/comment');
 const gravatar = require('gravatar');
+const CONFIG = require('../config');
 const md = require('markdown-it')();
+const md5 = require('md5');
 
 /**
- * 添加评论 API
+ * 添加评论列表 API
  * @param {*} ctx
  */
-async function addComment(ctx) {
+async function fetchAddComment(ctx) {
   try {
-    let data = await commentModel.find()
+    const { id, name, email, site, content } = ctx.request.body;
+    let avatar = md5(email).toLowerCase();
+    let getAvatar = gravatar.url(avatar, {
+        s: CONFIG.AVATAR.size,
+        r: CONFIG.AVATAR.r,
+        d: CONFIG.AVATAR.d
+      },
+      true);
+    await commentModel.create({
+      artId: id,
+      from_user: name,
+      from_email: email,
+      from_webSite: site,
+      from_avatar: getAvatar,
+      from_content: content
+    }).then(res => {
+      let result = {
+        id: res.id,
+        from_user: res.from_user,
+        from_email: res.from_email,
+        from_avatar: res.from_avatar,
+        from_content: res.from_content,
+        like: res.like,
+        from_date: res.from_date
+      }
+      ctx.body = {
+        code: 1,
+        error: 0,
+        result,
+        msg: "添加评论成功",
+      }
+    })
   } catch (err) {
     ctx.body = {
       error: 1,
@@ -27,6 +60,42 @@ async function addComment(ctx) {
   }
 }
 
+async function fetchComment(ctx) {
+  try {
+    const { id } = ctx.request.body;
+    let result = await commentModel.find({
+      artId: id
+    }, {
+      _id: 0,
+      id: 1,
+      from_user: 1,
+      from_email: 1,
+      from_webSite: 1,
+      from_avatar: 1,
+      from_content: 1,
+      like: 1,
+      from_date: 1
+    }, {
+      sort: {
+        id: -1
+      }
+    })
+    ctx.body = {
+      code: 1,
+      error: 0,
+      result,
+      msg: "获取评论列表成功",
+    }
+  } catch (err) {
+    ctx.body = {
+      error: 1,
+      msg: "获取评论列表失败",
+      err
+    }
+  }
+}
+
 module.exports = {
-  addComment
+  fetchComment,
+  fetchAddComment
 };
--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@
  * @Github: https://github.com/MoonCheung
  * @Date: 2019-12-12 23:38:34
  * @LastEditors: MoonCheung
- * @LastEditTime: 2019-12-13 00:44:09
+ * @LastEditTime: 2019-12-25 23:24:32
  */
 
 const autoIncrement = require("mongoose-auto-increment");
@@ -22,19 +22,34 @@ let CommentSchema = new Schema({
     ref: "id"
   },
   artId: Number,
-  content: String,
-  oldContent: String,
-  from_user: String,
-  from_email: String,
-  from_weblink: String,
+  from_user: {
+    type: String,
+    required: true
+  },
+  from_email: {
+    type: String,
+    required: true
+  },
+  from_webSite: String,
   from_avatar: String,
-  to_user: String,
-  to_email: String,
-  to_weblink: String,
-  to_avatar: String,
-  cdate: {
+  from_content: {
+    type: String,
+    required: true
+  },
+  from_date: {
     type: Date,
     default: Date.now
+  },
+  to_user: String,
+  to_email: String,
+  to_webSite: String,
+  to_avatar: String,
+  to_content: String,
+  to_date: Date,
+  // 点赞数
+  like: {
+    type: Number,
+    default: 0
   }
 });
 
@@ -45,4 +60,4 @@ CommentSchema.plugin(autoIncrement.plugin, {
   incrementBy: 1
 });
 CountersSchema.plugin(autoIncrement.plugin, "id");
-module.exports = DB.model("comment", ArticleSchema);
+module.exports = DB.model("comment", CommentSchema);
